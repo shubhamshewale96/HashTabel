4384f9de5feb013aaeabb775281de5de83dd8a8c
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-12 16:21:22 +0530

    add:ability to find frequency of word in sentance

--- src/main/java/com/bl/HashTabel/HashTabel.java
+++
@@ -1,7 +0,0 @@
-package com.bl.HashTabel;
-
-public class HashTabel {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hash Tabel");
-	}
-}
---
+++ src/main/java/com/bl/HashTabel/INode.java
@@ -0,0 +1,12 @@
+package com.bl.HashTabel;
+
+public interface INode<K> {
+	K getKey();
+
+    void setKey(K key);
+
+    INode getNext();
+
+    void setNext(INode next);
+
+}
